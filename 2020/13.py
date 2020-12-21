62a3f9e6734aacedd11335affde8e10cca21516a
Garrett Hoffman <garrett.lee.hoffman@gmail.com>
2020-12-20 22:09:49 -0500

    add day 13 part 2 solution

--- 2020/13.py
+++ 2020/13.py
@@ -5,6 +5,29 @@ from typing import List
 from utils.io import read_input_blob
 
 
+# NOT MY CODE : https://rosettacode.org/wiki/Chinese_remainder_theorem
+from functools import reduce
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+ 
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+#  NOT MY CODE ENDS HERE
+
+
 def part_one(input_file: str):
     # read and parse input file
     inputs = read_input_blob(input_file)
@@ -27,8 +50,24 @@ def part_one(input_file: str):
 
 
 def part_two(input_file: str):
-    raise NotImplementedError
-    
+    # read and parse input file
+    inputs = read_input_blob(input_file)
+    _, raw_buses = inputs.split('\n')
+
+    # process inputs and get diffs for crt
+    buses = [int(bus) if bus[0] != 'x' else 0 for bus in raw_buses.split(",")]
+    diffs = [bus - i for i, bus in enumerate(buses)]
+
+    # remove the 0s (formerly xs) from buses and difs
+    rel_buses = []
+    rel_diffs = []
+    for i, bus in enumerate(buses):
+        if bus > 0:
+            rel_buses.append(bus)
+            rel_diffs.append(diffs[i])
+
+    return chinese_remainder(rel_buses, rel_diffs)
+
 
 def main():
     try:
